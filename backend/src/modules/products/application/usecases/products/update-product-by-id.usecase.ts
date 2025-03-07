6a05e8ddf335c62816a275cb559992306225a53b
rod <dev_rodts@outlook.com>
2025-03-07 10:12:18 -0300

    "refactor: refactoring updateProduct."

--- backend/src/modules/products/application/usecases/products/update-product-by-id.usecase.ts
+++ backend/src/modules/products/application/usecases/products/update-product-by-id.usecase.ts
@@ -1,21 +1,21 @@
+import { CreateProductDTO } from "src/modules/products/domain/interface/dtos/products/create-product.dto";
 import { ProductsService } from "../../services/products/products.service";
-import { UpdateProductDTO } from "src/modules/products/domain/interface/dtos/products/update-product.dto";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { UpdateProductDTO } from "src/modules/products/domain/interface/dtos/products";
+
+@Injectable()
 export class UpdateProductByIdUseCase{
     constructor(
         private readonly productsService: ProductsService
     ){}
     
-    async execute(dto: UpdateProductDTO){
-        try{
-            const product = await this.productsService.updateProductById(dto.id, dto);
-            if(!product){
-                console.log("Product not found :::::", product)
-                return null;
-            }
-            return product;
-        }catch(error){
-            console.log("UpdateProductByIdUseCase :::::", error)
-            throw error;
+    async execute(data: { id: string; dto: Partial<UpdateProductDTO> }) {
+        const { id, dto } = data;
+        try {
+          return this.productsService.updateProductById(id, dto);
+        } catch (error) {
+          console.log("UpdateProductByIdUseCase :::::: CATCH", error);
+          throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
         }
-    }
+      }
 }
